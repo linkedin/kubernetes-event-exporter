8775bdffe28c4cbdf7cd40454a3ec4777460671f
Ronak Nathani <7279934+ronaknnathani@users.noreply.github.com>
2024-05-07 10:57:31 -0400

    Use greater of creationTimeStamp and lastTimestamp to determine event age
    Currently, kubernetes-events-exporter looks at lastTimestamp (eventTime if lastTimestamp is missing)
    of the event to determine the event age. If event age exceeded maxEventAgeSeconds, the event
    is discarded. While based on event aggregation, lastTimestamp should be greater than creationTimestamp,
    in practice it doesn't always seem to be the case. Hence, the current way of determining eventAge
    sometimes results in events younger than maxEventAgeSeconds not getting processed. So as part of this
    change, we use the greater of creationTimestamp and lastTimestamp (or eventTime if lastTimestamp is empty
    and firstTimestamp if eventTime is empty) to calculate event age. This allows us to capture more
    events rather than less and even if an event is processed more than once, we can de-dup it during
    analysis by using a distinct operation on ResourceVersion.
    
    **How event aggregation works at a high level**
    At a high level, client-go maintains a local events cache. When a new event comes, it correlates
    this event with the ones in cache. If it finds a matching event in the cache, it just updates the
    event count and the message (prefixes message with "(combined from similar events) $original-message")
    and patches the existing event instead of creating a new one. This is an optimization to reduce the
    storage needed to persist events in ETCD. Additionally, events have a TTL set (defaults to 1hr). So,
    if client-go tries to patch an event and the event has already expired, it creates a new event in this
    case. In such scenarios, the lastTimestamp of the event is kept from what was in the cache and the
    creationTimestamp is set by the apiserver when the event is created. While technically the delta between
    lastTimestamp and creationTimestamp should be very small, in practice, the delta is seen up to a few minutes.
    We haven't exactly found why that is the case but a probable reason is that before the event is created, the
    request to apiserver is queued.
    
    References:
    * Here's an explanation of how event aggregation works - https://dev.to/shuheiktgw/kubernetes-event-aggregation-and-spam-filtering-in-client-go-25mn
    * Event aggregation: https://github.com/kubernetes/kubernetes/blob/d9c7616d4b2f563c055df1f378d4dbeed33af869/staging/src/k8s.io/client-go/tools/record/events_cache.go#L242
    * Event update: https://github.com/kubernetes/kubernetes/blob/d9c7616d4b2f563c055df1f378d4dbeed33af869/staging/src/k8s.io/client-go/tools/record/events_cache.go#L327

--- pkg/kube/watcher_test.go
+++ pkg/kube/watcher_test.go
@@ -130,13 +130,15 @@ func TestEventWatcher_EventAge_whenEventCreatedAfterStartupAndBeforeMaxAge(t *te
 
 	startup := time.Now().Add(-10 * time.Minute)
 	ew.setStartUpTime(startup)
-	// event is 8m after stratup time (2m in max age) -> expect processed
+	// event is 8m after startup time (2m in max age) -> expect processed
 	event1 := corev1.Event{
+		ObjectMeta: metav1.ObjectMeta{
+			CreationTimestamp: metav1.Time{Time: startup.Add(8 * time.Minute)},
+		},
 		InvolvedObject: corev1.ObjectReference{
 			UID:  "test",
 			Name: "test-1",
 		},
-		LastTimestamp: metav1.Time{Time: startup.Add(8 * time.Minute)},
 	}
 
 	assert.False(t, ew.isEventDiscarded(&event1))
@@ -145,39 +147,6 @@ func TestEventWatcher_EventAge_whenEventCreatedAfterStartupAndBeforeMaxAge(t *te
 	assert.Contains(t, output.String(), "test-1")
 	assert.Contains(t, output.String(), "Received event")
 	assert.Equal(t, float64(1), testutil.ToFloat64(metricsStore.EventsProcessed))
-
-	// event is 8m after stratup time (2m in max age) -> expect processed
-	event2 := corev1.Event{
-		InvolvedObject: corev1.ObjectReference{
-			UID:  "test",
-			Name: "test-2",
-		},
-		EventTime: metav1.MicroTime{Time: startup.Add(8 * time.Minute)},
-	}
-
-	assert.False(t, ew.isEventDiscarded(&event2))
-	assert.NotContains(t, output.String(), "Event discarded as being older then maxEventAgeSeconds")
-	ew.onEvent(&event2)
-	assert.Contains(t, output.String(), "test-2")
-	assert.Contains(t, output.String(), "Received event")
-	assert.Equal(t, float64(2), testutil.ToFloat64(metricsStore.EventsProcessed))
-
-	// event is 8m after stratup time (2m in max age) -> expect processed
-	event3 := corev1.Event{
-		InvolvedObject: corev1.ObjectReference{
-			UID:  "test",
-			Name: "test-3",
-		},
-		LastTimestamp: metav1.Time{Time: startup.Add(8 * time.Minute)},
-		EventTime:     metav1.MicroTime{Time: startup.Add(8 * time.Minute)},
-	}
-
-	assert.False(t, ew.isEventDiscarded(&event3))
-	assert.NotContains(t, output.String(), "Event discarded as being older then maxEventAgeSeconds")
-	ew.onEvent(&event3)
-	assert.Contains(t, output.String(), "test-3")
-	assert.Contains(t, output.String(), "Received event")
-	assert.Equal(t, float64(3), testutil.ToFloat64(metricsStore.EventsProcessed))
 
 	metrics.DestroyMetricsStore(metricsStore)
 }
@@ -194,40 +163,15 @@ func TestEventWatcher_EventAge_whenEventCreatedAfterStartupAndAfterMaxAge(t *tes
 	startup := time.Now().Add(-10 * time.Minute)
 	ew.setStartUpTime(startup)
 	event1 := corev1.Event{
-		ObjectMeta:    metav1.ObjectMeta{Name: "event1"},
-		LastTimestamp: metav1.Time{Time: startup.Add(3 * time.Minute)},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:              "event1",
+			CreationTimestamp: metav1.Time{Time: startup.Add(3 * time.Minute)},
+		},
 	}
 	assert.True(t, ew.isEventDiscarded(&event1))
 	assert.Contains(t, output.String(), "event1")
-	assert.Contains(t, output.String(), "Event discarded as being older then maxEventAgeSeconds")
+	assert.Contains(t, output.String(), "Event discarded as being older than maxEventAgeSeconds")
 	ew.onEvent(&event1)
-	assert.NotContains(t, output.String(), "Received event")
-	assert.Equal(t, float64(0), testutil.ToFloat64(metricsStore.EventsProcessed))
-
-	// event is 3m after stratup time (and 2m after max age) -> expect dropped with warn
-	event2 := corev1.Event{
-		ObjectMeta: metav1.ObjectMeta{Name: "event2"},
-		EventTime:  metav1.MicroTime{Time: startup.Add(3 * time.Minute)},
-	}
-
-	assert.True(t, ew.isEventDiscarded(&event2))
-	assert.Contains(t, output.String(), "event2")
-	assert.Contains(t, output.String(), "Event discarded as being older then maxEventAgeSeconds")
-	ew.onEvent(&event2)
-	assert.NotContains(t, output.String(), "Received event")
-	assert.Equal(t, float64(0), testutil.ToFloat64(metricsStore.EventsProcessed))
-
-	// event is 3m after stratup time (and 2m after max age) -> expect dropped with warn
-	event3 := corev1.Event{
-		ObjectMeta:    metav1.ObjectMeta{Name: "event3"},
-		LastTimestamp: metav1.Time{Time: startup.Add(3 * time.Minute)},
-		EventTime:     metav1.MicroTime{Time: startup.Add(3 * time.Minute)},
-	}
-
-	assert.True(t, ew.isEventDiscarded(&event3))
-	assert.Contains(t, output.String(), "event3")
-	assert.Contains(t, output.String(), "Event discarded as being older then maxEventAgeSeconds")
-	ew.onEvent(&event3)
 	assert.NotContains(t, output.String(), "Received event")
 	assert.Equal(t, float64(0), testutil.ToFloat64(metricsStore.EventsProcessed))
 
@@ -247,8 +191,10 @@ func TestOnEvent_WithObjectMetadata(t *testing.T) {
 	startup := time.Now().Add(-10 * time.Minute)
 	ew.setStartUpTime(startup)
 	event1 := corev1.Event{
-		ObjectMeta:    metav1.ObjectMeta{Name: "event1"},
-		LastTimestamp: metav1.Time{Time: startup.Add(8 * time.Minute)},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:              "event1",
+			CreationTimestamp: metav1.Time{Time: startup.Add(8 * time.Minute)},
+		},
 		InvolvedObject: corev1.ObjectReference{
 			UID:  "test",
 			Name: "test-1",
@@ -284,8 +230,10 @@ func TestOnEvent_DeletedObjects(t *testing.T) {
 	startup := time.Now().Add(-10 * time.Minute)
 	ew.setStartUpTime(startup)
 	event1 := corev1.Event{
-		ObjectMeta:    metav1.ObjectMeta{Name: "event1"},
-		LastTimestamp: metav1.Time{Time: startup.Add(8 * time.Minute)},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:              "event1",
+			CreationTimestamp: metav1.Time{Time: startup.Add(8 * time.Minute)},
+		},
 		InvolvedObject: corev1.ObjectReference{
 			UID:  "test",
 			Name: "test-1",
